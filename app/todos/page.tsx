08f0fa9afe770a366c0f7f0749de62fbff747c0a
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2023-10-03 10:29:03 +0200

    initial commit

---
+++ app/todos/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+
+type Todo = {
+  id: string;
+  created_at: string;
+  title: string;
+  is_complete: boolean;
+  user_id: string;
+};
+
+export default function ClientComponent() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // Create a Supabase client configured to use cookies
+  const supabase = createClientComponentClient();
+
+  useEffect(() => {
+    const getTodos = async () => {
+      // This assumes you have a `todos` table in Supabase. Check out
+      // the `Create Table and seed with data` section of the README 👇
+      // https://github.com/vercel/next.js/blob/canary/examples/with-supabase/README.md
+      const { data } = await supabase.from("todos").select();
+      if (data) {
+        setTodos(data);
+      }
+    };
+
+    getTodos();
+  }, [supabase, setTodos]);
+
+  return (
+    <div className="space-y-2">
+      {todos.map((item) => (
+        <div className="text-white rounded-lg border p-6 w-96 relative">
+          <div className="flex gap-4">{item.title}</div>
+          <div className="text-slate-200 my-2 text-sm">Not assigned</div>
+          <div className="text-right text-sm">
+            {new Date(item.created_at).toDateString()}
+            {"\t"}
+            <span>{item.is_complete ? "✅" : "❌"}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
