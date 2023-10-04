478bb3487fb99e05723b0a1f04d1068181310a25
khaya-zulu <39437696+khaya-zulu@users.noreply.github.com>
2023-10-04 21:10:43 +0200

    update doc urls

--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,17 @@ export const dynamic = "force-dynamic";
 const resources = [
   {
     title: "Create a Snaplet account",
-    subtitle: "",
-    url: "",
+    url: "https://app.snaplet.dev/",
     icon: "M7 4V20M17 4V20M3 8H7M17 8H21M3 12H21M3 16H7M17 16H21M4 20H20C20.5523 20 21 19.5523 21 19V5C21 4.44772 20.5523 4 20 4H4C3.44772 4 3 4.44772 3 5V19C3 19.5523 3.44772 20 4 20Z",
   },
   {
     title: "Restore to local development supabase stack",
-    subtitle: "",
-    url: "",
+    url: "https://docs.snaplet.dev/recipes/supabase#restoring-to-a-supabase-local-development-stack",
     icon: "M10 20L14 4M18 8L22 12L18 16M6 16L2 12L6 8",
   },
   {
     title: "Seed a local development supabase stack",
-    subtitle: "",
-    url: "",
+    url: "https://docs.snaplet.dev/recipes/supabase#generate-data-for-your-supabase-local-development-stack",
     icon: "M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528",
   },
 ];
@@ -108,7 +105,7 @@ export default async function Index() {
             Everything you need to get started
           </h2>
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-            {resources.map(({ title, subtitle, url, icon }) => (
+            {resources.map(({ title, url, icon }) => (
               <a
                 key={title}
                 className="relative flex flex-col group rounded-lg border p-6 hover:border-foreground"
@@ -120,7 +117,6 @@ export default async function Index() {
                   {title}
                 </h3>
                 <div className="flex flex-col grow gap-4 justify-between">
-                  <p className="text-sm opacity-70">{subtitle}</p>
                   <div className="flex justify-between items-center">
                     <svg
                       width="24"
